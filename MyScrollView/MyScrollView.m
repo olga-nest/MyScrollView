f88606d4499a41250cf0741960a9e328df48fe2f
olga-nest <olga.w.nesterova@gmail.com>
2017-10-16 13:04:35 -0700

    Add MyScrollView class

---
+++ MyScrollView/MyScrollView.m
@@ -0,0 +1,14 @@
+//
+//  MyScrollView.m
+//  MyScrollView
+//
+//  Created by Olga on 10/16/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import "MyScrollView.h"
+
+@implementation MyScrollView
+
+
+@end
